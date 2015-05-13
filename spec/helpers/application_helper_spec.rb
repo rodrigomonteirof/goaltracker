642aa7495541a409d46825a5d3d8dab6dda57216
Rodrigo Monteiro Ferreira <rodrigomonteirof@gmail.com>
2015-05-13 17:56:12 -0300

    remove deprecated stub

--- spec/helpers/application_helper_spec.rb
+++ spec/helpers/application_helper_spec.rb
@@ -6,7 +6,7 @@ describe ApplicationHelper do
 
     context 'when user is logged' do
       before do
-        stub(:user_signed_in?).and_return(true)
+        allow(self).to receive(:user_signed_in?).and_return(true)
       end
 
       it { is_expected.to eq '<a href="/logout">Sair</a>' }
@@ -14,11 +14,10 @@ describe ApplicationHelper do
 
     context 'when user isnt logged' do
       before do
-        stub(:user_signed_in?).and_return(false)
+         allow(self).to receive(:user_signed_in?).and_return(false)
       end
 
       it { is_expected.to eq '<a href="auth/facebook">Login Facebook</a> | <a href="auth/google_oauth2">Login Google</a>' }
-
     end
   end
 end
